46e21c64cfb2d3acc9122c5f4d4cedd6a67d0441
HappyFaceIndustries <creeperweirdo123@gmail.com>
2015-06-10 17:35:13 -0500

    Yay!
    :octopus: :

---
+++ Source/ScreenMessageHider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace ScreenMessageHider
+{
+	[KSPAddon(KSPAddon.Startup.MainMenu, true)]
+	public class ScreenMessageHider : MonoBehaviour
+	{
+		void Start()
+		{
+			GameEvents.onShowUI.Add (onShowUI);
+			GameEvents.onHideUI.Add (onHideUI);
+		}
+		void onShowUI()
+		{
+			ScreenMessages sm = GameObject.FindObjectOfType<ScreenMessages> ();
+			if (sm != null)
+				sm.enabled = true;
+		}
+		void onHideUI()
+		{
+			ScreenMessages sm = GameObject.FindObjectOfType<ScreenMessages> ();
+			if (sm != null)
+				sm.enabled = false;
+		}
+	}
+}
