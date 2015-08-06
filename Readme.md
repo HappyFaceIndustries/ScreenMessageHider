8d5ff3153af5c4dbd01a279d28652a52c4117235
HappyFaceIndustries <creeperweirdo123@gmail.com>
2015-08-05 19:38:29 -0500

    Restructuring of directories

---
+++ Readme.md
@@ -0,0 +1,10 @@
+#Screen Message Hider
+
+Forum thread: ~~forum.kerbalspaceprogram.com/~~
+
+ScreenMessageHider is a mod that hides screen messages that don't normally get hidden, like time warp notifications, when you hide the UI with F2
+
+To install:
+
+1. Merge GameData folder with the one in your KSP folder
+2. Done!
---
+++ ScreenMessageHider/ScreenMessageHider.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+namespace ScreenMessageHider
+{
+	[KSPAddon(KSPAddon.Startup.Instantly, false)]
+	public class ScreenMessageHider : MonoBehaviour
+	{
+		void Start()
+		{
+			GameEvents.onHideUI.Add (OnHideUI);
+			GameEvents.onShowUI.Add (OnShowUI);
+		}
+
+		/*
+		 *  Seriously Squad, why is this not stock?
+		 */
+
+		static ScreenMessages messages;
+		void OnHideUI()
+		{
+			if(messages == null)
+				messages = UnityEngine.Object.FindObjectOfType<ScreenMessages> ();
+			if (messages != null)
+				messages.enabled = false;
+		}
+		void OnShowUI()
+		{
+			if(messages == null)
+				messages = UnityEngine.Object.FindObjectOfType<ScreenMessages> ();
+			if (messages != null)
+				messages.enabled = true;
+		}
+	}
+}
+
